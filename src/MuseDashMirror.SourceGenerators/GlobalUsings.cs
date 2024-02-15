d0381ef16a9b6e8e5d644b45cd6496955b7decfe
lxymahatma <326265648@qq.com>
2024-02-15 12:29:04 +0800

    move enum to MuseDashMirror.Core

--- src/MuseDashMirror.SourceGenerators/GlobalUsings.cs
+++ src/MuseDashMirror.SourceGenerators/GlobalUsings.cs
@@ -2,4 +2,5 @@ global using Microsoft.CodeAnalysis;
 global using Microsoft.CodeAnalysis.CSharp;
 global using Microsoft.CodeAnalysis.CSharp.Syntax;
 global using Microsoft.CodeAnalysis.Diagnostics;
+global using MuseDashMirror.Core;
 global using static MuseDashMirror.SourceGenerators.AttributeFullNames;
