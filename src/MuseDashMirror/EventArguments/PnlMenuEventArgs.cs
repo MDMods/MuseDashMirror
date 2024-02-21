530c5ccd9fe8dc3ff366fb893e6232f6a130828e
lxymahatma <326265648@qq.com>
2024-02-21 13:07:52 +0800

    use sealed

--- src/MuseDashMirror/EventArguments/PnlMenuEventArgs.cs
+++ src/MuseDashMirror/EventArguments/PnlMenuEventArgs.cs
@@ -2,7 +2,7 @@ using Il2CppAssets.Scripts.UI.Panels;
 
 namespace MuseDashMirror.EventArguments;
 
-public class PnlMenuEventArgs(PnlMenu pnlMenu) : EventArgs
+public sealed class PnlMenuEventArgs(PnlMenu pnlMenu) : EventArgs
 {
     public PnlMenu PnlMenu { get; set; } = pnlMenu;
 }
--- src/MuseDashMirror/EventArguments/PnlStageEventArgs.cs
+++ src/MuseDashMirror/EventArguments/PnlStageEventArgs.cs
@@ -2,7 +2,7 @@ using Il2CppAssets.Scripts.UI.Panels;
 
 namespace MuseDashMirror.EventArguments;
 
-public class PnlStageEventArgs(PnlStage pnlStage) : EventArgs
+public sealed class PnlStageEventArgs(PnlStage pnlStage) : EventArgs
 {
     public PnlStage PnlStage { get; set; } = pnlStage;
 }
