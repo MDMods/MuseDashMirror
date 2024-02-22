292cc21aa3bf8310944c9b4141d50c7d4ce3b451
lxymahatma <326265648@qq.com>
2024-02-22 11:23:18 +0800

    update xml documentation

--- src/MuseDashMirror/EventArguments/SwitchLanguagesEventArgs.cs
+++ src/MuseDashMirror/EventArguments/SwitchLanguagesEventArgs.cs
@@ -2,7 +2,14 @@ using Il2CppAssets.Scripts.UI.Specials;
 
 namespace MuseDashMirror.EventArguments;
 
+/// <summary>
+///     Event Arguments for <see cref="Il2CppAssets.Scripts.UI.Specials.SwitchLanguages" /> Patch
+/// </summary>
+/// <param name="switchLanguages"></param>
 public sealed class SwitchLanguagesEventArgs(SwitchLanguages switchLanguages) : EventArgs
 {
-    public SwitchLanguages SwitchLanguages { get; } = switchLanguages;
+    /// <summary>
+    ///     SwitchLanguages Instance
+    /// </summary>
+    public SwitchLanguages SwitchLanguages => switchLanguages;
 }
