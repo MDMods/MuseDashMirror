8e146ae7f08b414a3b049bd6a11d6d2098e6c32f
lxymahatma <326265648@qq.com>
2024-02-25 19:55:29 +0800

    draft for PnlMenuToggleGenerator

--- src/MuseDashMirror/Patch/PnlMenuPatch.cs
+++ src/MuseDashMirror/Patch/PnlMenuPatch.cs
@@ -5,5 +5,13 @@ namespace MuseDashMirror.Patch;
 [HarmonyPatch(typeof(PnlMenu), nameof(PnlMenu.Awake))]
 internal static class PnlMenuPatch
 {
-    private static void Postfix(PnlMenu __instance) => PnlMenuPatchInvoke(__instance);
+    private static void Postfix(PnlMenu __instance)
+    {
+        if (!GameObjectCache.ContainsKey("TglOn"))
+        {
+            GameObjectCache["TglOn"] = GameObject.Find("Forward").transform.Find("PnlVolume").Find("LogoSetting").GetChild(2).GetChild(0).gameObject;
+        }
+
+        PnlMenuPatchInvoke(__instance);
+    }
 }
