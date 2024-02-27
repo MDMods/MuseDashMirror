44ebd27c36c2c7a1d60b1c8f5a103cf4eb6b7e42
lxymahatma <326265648@qq.com>
2024-02-27 23:58:36 +0800

    update methods

--- src/MuseDashMirror/Extensions/GameObjectExtensions.cs
+++ src/MuseDashMirror/Extensions/GameObjectExtensions.cs
@@ -1,4 +1,5 @@
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 namespace MuseDashMirror.Extensions;
 
@@ -16,7 +17,23 @@ public static partial class GameObjectExtensions
     public static void SetParent(this GameObject gameObject, GameObject parent) => gameObject.transform.SetParent(parent.transform);
 
     /// <summary>
-    ///     Set the Text component of a GameObject
+    ///     Set the text of a GameObject with a Text gameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject</param>
+    /// <param name="text">Text</param>
+    public static void SetText(this GameObject gameObject, string text)
+    {
+        if (gameObject.GetComponent<Text>() == null)
+        {
+            Logger.Error($"GameObject {gameObject} does not have a Text component");
+            return;
+        }
+
+        gameObject.GetComponent<Text>().text = text;
+    }
+
+    /// <summary>
+    ///     Set the Text gameObject of a GameObject
     /// </summary>
     /// <param name="gameObject">GameObject</param>
     /// <param name="textParameters">Text Parameters</param>
@@ -33,8 +50,8 @@ public static partial class GameObjectExtensions
     /// <summary>
     ///     Set the RectTransform of a GameObject
     /// </summary>
-    /// <param name="gameObject"></param>
-    /// <param name="transformParameters"></param>
+    /// <param name="gameObject">GameObject</param>
+    /// <param name="transformParameters">Transform Parameters</param>
     public static void SetRectTransform(this GameObject gameObject, TransformParameters transformParameters)
     {
         var rectTransform = gameObject.GetComponent<RectTransform>();
@@ -42,25 +59,23 @@ public static partial class GameObjectExtensions
 
         if (transformParameters.IsAutoSize)
         {
-            var contentSizeFitter = gameObject.AddComponent<ContentSizeFitter>();
-            contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
-            contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            gameObject.AddContentSizeFitter();
         }
         else
         {
             rectTransform.sizeDelta = transformParameters.SizeDelta;
         }
 
-        LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+        rectTransform.UpdateTransformLayoutInfo();
         transformParameters.PositionStrategy.SetPosition(rectTransform, transformParameters);
     }
 
     /// <summary>
-    ///     Find a component in the ancestors of a GameObject
+    ///     Find a Component in the ancestors of a GameObject
     /// </summary>
-    /// <param name="gameObject"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
+    /// <param name="gameObject">GameObject</param>
+    /// <typeparam name="T">Component</typeparam>
+    /// <returns>Component</returns>
     public static T FindComponentInAncestors<T>(this GameObject gameObject) where T : Component
     {
         while (true)
@@ -81,4 +96,43 @@ public static partial class GameObjectExtensions
             gameObject = parent.gameObject;
         }
     }
+
+    /// <summary>
+    ///     Get the total scaled factor of a GameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject</param>
+    /// <returns>Scaled Factor</returns>
+    public static float GetTotalScaledFactor(this GameObject gameObject)
+    {
+        var scaleFactor = 1f;
+        while (true)
+        {
+            scaleFactor *= gameObject.transform.localScale.x;
+
+            var parent = gameObject.transform.parent;
+            if (parent == null)
+            {
+                return scaleFactor;
+            }
+
+            gameObject = parent.gameObject;
+        }
+    }
+
+    /// <summary>
+    ///     Add a ContentSizeFitter to a GameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject</param>
+    public static void AddContentSizeFitter(this GameObject gameObject)
+    {
+        var contentSizeFitter = gameObject.AddComponent<ContentSizeFitter>();
+        contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+        contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+    }
+
+    /// <summary>
+    ///     Destroy a GameObject
+    /// </summary>
+    /// <param name="gameObject">GameObject</param>
+    public static void Destroy(this GameObject gameObject) => Object.Destroy(gameObject);
 }
--- src/MuseDashMirror/Patch/PnlMenuPatch.cs
+++ src/MuseDashMirror/Patch/PnlMenuPatch.cs
@@ -12,6 +12,11 @@ internal static class PnlMenuPatch
             GameObjectCache["TglOn"] = GameObject.Find("Forward").transform.Find("PnlVolume").Find("LogoSetting").GetChild(2).GetChild(0).gameObject;
         }
 
+        if (!GameObjectCache.ContainsKey("PnlMenu"))
+        {
+            GameObjectCache["PnlMenu"] = __instance.gameObject;
+        }
+
         PnlMenuPatchInvoke(__instance);
     }
 }
