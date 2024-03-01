b71dfc4fbcd817a31efb090087d6425bc1c0171d
lxymahatma <326265648@qq.com>
2024-03-01 14:50:33 +0800

    move get location into utils

--- src/MuseDashMirror.SourceGenerators/Utils.cs
+++ src/MuseDashMirror.SourceGenerators/Utils.cs
@@ -2,6 +2,9 @@ namespace MuseDashMirror.SourceGenerators;
 
 internal static class Utils
 {
+    internal static Location GetClassDeclarationLocation(ClassDeclarationSyntax classDeclaration) =>
+        Location.Create(classDeclaration.SyntaxTree, TextSpan.FromBounds(classDeclaration.Keyword.Span.Start, classDeclaration.Identifier.Span.End));
+
     internal static LocalizableString GetLocalizableString(string name) =>
         new LocalizableResourceString(name, ResourceManager, typeof(Resources));
 }
