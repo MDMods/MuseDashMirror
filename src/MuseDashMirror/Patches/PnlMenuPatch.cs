d62326cd6239e0ff2612e3ea30c46e2a945b754a
lxymahatma <326265648@qq.com>
2024-03-14 11:50:10 +0800

    :bug: fix toggle error

--- src/MuseDashMirror/Patches/PnlMenuPatch.cs
+++ src/MuseDashMirror/Patches/PnlMenuPatch.cs
@@ -7,11 +7,6 @@ internal static class PnlMenuPatch
 {
     private static void Postfix(PnlMenu __instance)
     {
-        if (!GameObjectCache.ContainsKey("TglOn"))
-        {
-            GameObjectCache["TglOn"] = GameObject.Find("Forward").transform.Find("PnlVolume").Find("LogoSetting").GetChild(2).GetChild(0).gameObject;
-        }
-
         GameObjectCache["PnlMenu"] = __instance.gameObject;
         GameObjectCache["PnlOption"] = __instance.transform.GetChild(2).GetChild(3).gameObject;
 
