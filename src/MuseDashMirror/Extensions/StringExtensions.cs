02801f75d79782e4fbf11dfe4fcac48d49c23959
lxymahatma <326265648@qq.com>
2024-03-17 11:52:49 +0800

    :fire: remove unneeded code

--- src/MuseDashMirror/Extensions/StringExtensions.cs
+++ src/MuseDashMirror/Extensions/StringExtensions.cs
@@ -6,29 +6,6 @@ namespace MuseDashMirror.Extensions;
 public static class StringExtensions
 {
     /// <summary>
-    ///     Get visible text with ellipsis
-    /// </summary>
-    /// <param name="originalText">Original Text</param>
-    /// <param name="inVisibleTextRange">The range of text to be replaced by ellipsis</param>
-    /// <returns>Result String</returns>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static string GetVisibleTextWithEllipsis(this string originalText, Range inVisibleTextRange)
-    {
-        var startValue = inVisibleTextRange.Start.Value;
-        var endValue = inVisibleTextRange.End.Value;
-        if (originalText.Length < startValue + endValue)
-        {
-            throw new ArgumentOutOfRangeException(nameof(inVisibleTextRange), inVisibleTextRange,
-                @"The range is out of the bounds of the original text");
-        }
-
-        var prefixText = originalText[..startValue];
-        var suffixText = originalText[^endValue..];
-
-        return $"{prefixText}...{suffixText}";
-    }
-
-    /// <summary>
     ///     Try to get visible text with ellipsis<br />
     ///     If the range is out of the bounds of the original text, the original text will be returned
     /// </summary>
