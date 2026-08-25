728c01cb73bd139325a6eb95555567f211c18271
lxymahatma <lxymahatma@gmail.com>
2026-08-25 18:55:41 +0800

    use extension blocks

--- src/MuseDashMirror/Extensions/UnityExtensions/GameObjectExtensions.cs
+++ src/MuseDashMirror/Extensions/UnityExtensions/GameObjectExtensions.cs
@@ -6,187 +6,179 @@ namespace MuseDashMirror.Extensions.UnityExtensions;
 [Logger]
 public static partial class GameObjectExtensions
 {
-    /// <summary>
-    ///     Get the Parent GameObject of a GameObject
-    /// </summary>
     /// <param name="gameObject">GameObject</param>
-    /// <returns>Parent GameObject</returns>
-    public static GameObject GetParentGameObject(this GameObject gameObject) => gameObject.GetParentTransform().gameObject;
-
-    /// <summary>
-    ///     Get the Parent Transform of a GameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <returns>Parent Transform</returns>
-    public static Transform GetParentTransform(this GameObject gameObject) => gameObject.transform.parent;
-
-    /// <summary>
-    ///     Set the Parent of a GameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="parent">Parent GameObject</param>
-    /// <param name="worldPositionStays">World Position Stays</param>
-    public static void SetParent(this GameObject gameObject, GameObject parent, bool worldPositionStays = true)
-        => gameObject.transform.SetParent(parent.transform, worldPositionStays);
-
-    /// <summary>
-    ///     Set the text of a GameObject with a Text gameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="text">Text</param>
-    public static void SetText(this GameObject gameObject, string text)
+    extension(GameObject gameObject)
     {
-        if (!gameObject.TryGetComponent<Text>(out var textComponent))
+        /// <summary>
+        ///     Get the Parent GameObject of a GameObject
+        /// </summary>
+        /// <returns>Parent GameObject</returns>
+        public GameObject GetParentGameObject() => gameObject.GetParentTransform().gameObject;
+
+        /// <summary>
+        ///     Get the Parent Transform of a GameObject
+        /// </summary>
+        /// <returns>Parent Transform</returns>
+        public Transform GetParentTransform() => gameObject.transform.parent;
+
+        /// <summary>
+        ///     Set the Parent of a GameObject
+        /// </summary>
+        /// <param name="parent">Parent GameObject</param>
+        /// <param name="worldPositionStays">World Position Stays</param>
+        public void SetParent(GameObject parent, bool worldPositionStays = true)
+            => gameObject.transform.SetParent(parent.transform, worldPositionStays);
+
+        /// <summary>
+        ///     Set the text of a GameObject with a Text gameObject
+        /// </summary>
+        /// <param name="text">Text</param>
+        public void SetText(string text)
         {
-            Logger.Error($"GameObject {gameObject} does not have a Text component");
-            return;
-        }
-
-        textComponent.text = text;
-    }
-
-    /// <summary>
-    ///     Set the color of a GameObject with a Text gameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="color">Color</param>
-    public static void SetColor(this GameObject gameObject, Color color)
-    {
-        if (!gameObject.TryGetComponent<Text>(out var textComponent))
-        {
-            Logger.Error($"GameObject {gameObject} does not have a Text component");
-            return;
-        }
-
-        textComponent.color = color;
-    }
-
-    /// <summary>
-    ///     Set the Text Component of a GameObject using Text Parameters
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="textParameters">Text Parameters</param>
-    public static void SetTextComponent(this GameObject gameObject, TextParameters textParameters)
-    {
-        var textComponent = gameObject.GetComponent<Text>() ?? gameObject.AddComponent<Text>();
-        textComponent.text = textParameters.GetText();
-        textComponent.font = textParameters.Font;
-        textComponent.fontSize = textParameters.FontSize;
-        textComponent.color = textParameters.Color;
-        textComponent.alignment = textParameters.Alignment;
-    }
-
-    /// <summary>
-    ///     Set the RectTransform of a GameObject using Transform Parameters
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="transformParameters">Transform Parameters</param>
-    public static void SetRectTransform(this GameObject gameObject, TransformParameters transformParameters)
-    {
-        var rectTransform = gameObject.GetComponent<RectTransform>();
-        rectTransform.localScale = transformParameters.LocalScale;
-
-        if (transformParameters.IsAutoSize)
-        {
-            gameObject.AddContentSizeFitter();
-        }
-        else
-        {
-            rectTransform.sizeDelta = transformParameters.SizeDelta;
-        }
-
-        rectTransform.UpdateTransformLayoutInfo();
-        transformParameters.PositionStrategy.SetPosition(rectTransform, transformParameters);
-    }
-
-    /// <summary>
-    ///     Find a Component in the ancestors of a GameObject including itself
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="includeSelf">Include Self</param>
-    /// <typeparam name="T">Component</typeparam>
-    /// <returns>Component</returns>
-    public static T FindComponentInAncestors<T>(this GameObject gameObject, bool includeSelf = true) where T : Component
-    {
-        var currentTransform = includeSelf ? gameObject.transform : gameObject.GetParentTransform();
-
-        while (currentTransform != null)
-        {
-            var component = currentTransform.gameObject.GetComponent<T>();
-            if (component != null)
+            if (!gameObject.TryGetComponent<Text>(out var textComponent))
             {
-                return component;
+                Logger.Error($"GameObject {gameObject} does not have a Text component");
+                return;
             }
 
-            currentTransform = currentTransform.parent;
+            textComponent.text = text;
         }
 
-        return null;
-    }
-
-    /// <summary>
-    ///     Try to find a Component in the ancestors of a GameObject including itself
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="component">Component</param>
-    /// <param name="includeSelf">Include Self</param>
-    /// <typeparam name="T">Component</typeparam>
-    /// <returns>Found</returns>
-    public static bool TryFindComponentInAncestors<T>(this GameObject gameObject, out T component, bool includeSelf = true) where T : Component
-    {
-        var currentTransform = includeSelf ? gameObject.transform : gameObject.GetParentTransform();
-
-        while (currentTransform != null)
+        /// <summary>
+        ///     Set the color of a GameObject with a Text gameObject
+        /// </summary>
+        /// <param name="color">Color</param>
+        public void SetColor(Color color)
         {
-            component = currentTransform.gameObject.GetComponent<T>();
-            if (component != null)
+            if (!gameObject.TryGetComponent<Text>(out var textComponent))
             {
-                return true;
+                Logger.Error($"GameObject {gameObject} does not have a Text component");
+                return;
             }
 
-            currentTransform = currentTransform.parent;
+            textComponent.color = color;
         }
 
-        component = null;
-        return false;
-    }
-
-    /// <summary>
-    ///     Get the total scale factor of a GameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <param name="includeSelf">Include Self</param>
-    /// <returns>Scale Factor Vector3</returns>
-    public static Vector3 GetTotalScaleFactor(this GameObject gameObject, bool includeSelf = true)
-    {
-        var scaleFactor = includeSelf ? gameObject.transform.localScale : Vector3.one;
-        var parentTransform = gameObject.GetParentTransform();
-
-        while (parentTransform != null)
+        /// <summary>
+        ///     Set the Text Component of a GameObject using Text Parameters
+        /// </summary>
+        /// <param name="textParameters">Text Parameters</param>
+        public void SetTextComponent(TextParameters textParameters)
         {
-            scaleFactor = Vector3.Scale(scaleFactor, parentTransform.localScale);
-            parentTransform = parentTransform.parent;
+            var textComponent = gameObject.GetComponent<Text>() ?? gameObject.AddComponent<Text>();
+            textComponent.text = textParameters.GetText();
+            textComponent.font = textParameters.Font;
+            textComponent.fontSize = textParameters.FontSize;
+            textComponent.color = textParameters.Color;
+            textComponent.alignment = textParameters.Alignment;
         }
 
-        return scaleFactor;
-    }
+        /// <summary>
+        ///     Set the RectTransform of a GameObject using Transform Parameters
+        /// </summary>
+        /// <param name="transformParameters">Transform Parameters</param>
+        public void SetRectTransform(TransformParameters transformParameters)
+        {
+            var rectTransform = gameObject.GetComponent<RectTransform>();
+            rectTransform.localScale = transformParameters.LocalScale;
 
-    /// <summary>
-    ///     Get the Canvas Scaler Factor of a GameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    /// <returns>Canvas Scaler Factor</returns>
-    public static float GetCanvasScalerFactor(this GameObject gameObject)
-        => gameObject.TryFindComponentInAncestors(out CanvasScaler canvasScaler) ? canvasScaler.referenceResolution.x / Screen.width : 1f;
+            if (transformParameters.IsAutoSize)
+            {
+                gameObject.AddContentSizeFitter();
+            }
+            else
+            {
+                rectTransform.sizeDelta = transformParameters.SizeDelta;
+            }
 
-    /// <summary>
-    ///     Add a ContentSizeFitter to a GameObject
-    /// </summary>
-    /// <param name="gameObject">GameObject</param>
-    public static void AddContentSizeFitter(this GameObject gameObject)
-    {
-        var contentSizeFitter = gameObject.AddComponent<ContentSizeFitter>();
-        contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
-        contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+            rectTransform.UpdateTransformLayoutInfo();
+            transformParameters.PositionStrategy.SetPosition(rectTransform, transformParameters);
+        }
+
+        /// <summary>
+        ///     Find a Component in the ancestors of a GameObject including itself
+        /// </summary>
+        /// <param name="includeSelf">Include Self</param>
+        /// <typeparam name="T">Component</typeparam>
+        /// <returns>Component</returns>
+        public T FindComponentInAncestors<T>(bool includeSelf = true) where T : Component
+        {
+            var currentTransform = includeSelf ? gameObject.transform : gameObject.GetParentTransform();
+
+            while (currentTransform != null)
+            {
+                var component = currentTransform.gameObject.GetComponent<T>();
+                if (component != null)
+                {
+                    return component;
+                }
+
+                currentTransform = currentTransform.parent;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        ///     Try to find a Component in the ancestors of a GameObject including itself
+        /// </summary>
+        /// <param name="component">Component</param>
+        /// <param name="includeSelf">Include Self</param>
+        /// <typeparam name="T">Component</typeparam>
+        /// <returns>Found</returns>
+        public bool TryFindComponentInAncestors<T>(out T component, bool includeSelf = true) where T : Component
+        {
+            var currentTransform = includeSelf ? gameObject.transform : gameObject.GetParentTransform();
+
+            while (currentTransform != null)
+            {
+                component = currentTransform.gameObject.GetComponent<T>();
+                if (component != null)
+                {
+                    return true;
+                }
+
+                currentTransform = currentTransform.parent;
+            }
+
+            component = null;
+            return false;
+        }
+
+        /// <summary>
+        ///     Get the total scale factor of a GameObject
+        /// </summary>
+        /// <param name="includeSelf">Include Self</param>
+        /// <returns>Scale Factor Vector3</returns>
+        public Vector3 GetTotalScaleFactor(bool includeSelf = true)
+        {
+            var scaleFactor = includeSelf ? gameObject.transform.localScale : Vector3.one;
+            var parentTransform = gameObject.GetParentTransform();
+
+            while (parentTransform != null)
+            {
+                scaleFactor = Vector3.Scale(scaleFactor, parentTransform.localScale);
+                parentTransform = parentTransform.parent;
+            }
+
+            return scaleFactor;
+        }
+
+        /// <summary>
+        ///     Get the Canvas Scaler Factor of a GameObject
+        /// </summary>
+        /// <returns>Canvas Scaler Factor</returns>
+        public float GetCanvasScalerFactor()
+            => gameObject.TryFindComponentInAncestors(out CanvasScaler canvasScaler) ? canvasScaler.referenceResolution.x / Screen.width : 1f;
+
+        /// <summary>
+        ///     Add a ContentSizeFitter to a GameObject
+        /// </summary>
+        public void AddContentSizeFitter()
+        {
+            var contentSizeFitter = gameObject.AddComponent<ContentSizeFitter>();
+            contentSizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            contentSizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+        }
     }
 }
--- src/MuseDashMirror/Extensions/UnityExtensions/TransformExtensions.cs
+++ src/MuseDashMirror/Extensions/UnityExtensions/TransformExtensions.cs
@@ -5,40 +5,42 @@ namespace MuseDashMirror.Extensions.UnityExtensions;
 /// </summary>
 public static class TransformExtensions
 {
-    /// <summary>
-    ///     Get the child of the <paramref name="transform" /> at the specified <paramref name="indexes" />
-    /// </summary>
     /// <param name="transform">Transform</param>
-    /// <param name="indexes">Indexes</param>
-    /// <returns>Transform</returns>
-    public static Transform GetChild(this Transform transform, params ReadOnlySpan<int> indexes)
+    extension(Transform transform)
     {
-        foreach (var index in indexes)
+        /// <summary>
+        ///     Get the child of the <paramref name="transform" /> at the specified <paramref name="indexes" />
+        /// </summary>
+        /// <param name="indexes">Indexes</param>
+        /// <returns>Transform</returns>
+        public Transform GetChild(params ReadOnlySpan<int> indexes)
         {
-            transform = transform.GetChild(index);
-        }
+            foreach (var index in indexes)
+            {
+                transform = transform.GetChild(index);
+            }
 
-        return transform;
-    }
-
-    /// <summary>
-    ///     Get the Ancestor Transform of a Transform with a specified number of levels up.
-    /// </summary>
-    /// <param name="transform">Transform</param>
-    /// <param name="ancestorLevels">The number of levels up to look for the ancestor<br />Must be greater than or equal to 1</param>
-    /// <returns>Ancestor Transform</returns>
-    public static Transform GetAncestorAtLevel(this Transform transform, int ancestorLevels = 1)
-    {
-        if (ancestorLevels < 1)
-        {
             return transform;
         }
 
-        while (ancestorLevels-- > 0 && transform.parent != null)
+        /// <summary>
+        ///     Get the Ancestor Transform of a Transform with a specified number of levels up.
+        /// </summary>
+        /// <param name="ancestorLevels">The number of levels up to look for the ancestor<br />Must be greater than or equal to 1</param>
+        /// <returns>Ancestor Transform</returns>
+        public Transform GetAncestorAtLevel(int ancestorLevels = 1)
         {
-            transform = transform.parent;
-        }
+            if (ancestorLevels < 1)
+            {
+                return transform;
+            }
 
-        return transform;
+            while (ancestorLevels-- > 0 && transform.parent != null)
+            {
+                transform = transform.parent;
+            }
+
+            return transform;
+        }
     }
 }
